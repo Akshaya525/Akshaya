7b86046ea495b4da57617ae141b48f200f3f1646
Akshaya525 <58969355+Akshaya525@users.noreply.github.com>
2019-12-31 15:06:05 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+if num1>=num2:
+    large=num1
+else:
+    large=num2
+    print("the largest number is:",large)
